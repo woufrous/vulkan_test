526b31efc87289527debf3ad737e225aaaa0d904
Hans Grob <H.Grob@physik.uni-muenchen.de>
2020-04-13 12:39:28 +0200

    OpenGL base project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+int main(int, char**) {
+    std::cout << "GLFW: " << glfwGetVersionString() << std::endl;
+    return 0;
+}
